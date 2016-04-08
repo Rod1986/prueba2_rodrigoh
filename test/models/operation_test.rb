8ebd020b4314dad767615a3f82bbc0b904fa54a0
Rodrigo <hein.rodrigo@gmail.com>
2016-04-08 20:32:14 -0300

    4th commit, operations model added

---
+++ test/models/operation_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class OperationTest < ActiveSupport::TestCase
+
+  def setup
+    @o = operations(:number_one)
+  end
+
+  test 'operation needs a worker' do
+    assert_not_nil @o.worker, 'a worker needs to be assigned'
+    end
+
+end
