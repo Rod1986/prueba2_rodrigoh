ef54e139854e035707eb3c90e034ca691c49b148
Rodrigo <hein.rodrigo@gmail.com>
2016-04-08 19:45:46 -0300

    3rd commit, workers model added

---
+++ test/models/worker_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class WorkerTest < ActiveSupport::TestCase
+ 
+  def setup
+    @w = workers(:alfred)
+  end
+
+  test 'invalid without a name' do
+    @w.name = nil
+    assert @w.invalid?, 'a name is needed'
+  end
+end
